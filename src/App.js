dea27eedcaf2f9d7398f805cbe288272307dedda
Brian <e.kallensp@gmail.com>
2021-09-12 21:05:10 -0300

    Navbar partially added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
-    <h1>Hola Mundo!</h1>
+    <div>
+      <Router>
+        <Navbar />
+      </Router>
+    </div>
   );
 }
 
